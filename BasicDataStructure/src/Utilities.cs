e4a8b27f40931effc1c5174fbf11b3697d1a5c7c
Ayaovi <ayaovi.djissenou@derivco.co.za>
2019-07-02 23:24:05 +0200

    add merge sort as part of utilities.

--- BasicDataStructure/src/Utilities.cs
+++ BasicDataStructure/src/Utilities.cs
@@ -1,24 +1,58 @@
 using System;
 
-namespace src
-{
-  public static class Utilities
-  {
-    public static LinkedList RemoveDuplicates(LinkedList list)
-    {
+namespace src {
+  public static class Utilities {
+    public static LinkedList RemoveDuplicates (LinkedList list) {
       var pointer1 = list.Head;
       var pointer2 = list.Head.Next;
 
-      while (pointer2 != null)
-      {
+      while (pointer2 != null) {
         if (pointer1.Data != pointer2.Data) {
           pointer1.Next = pointer2;
           pointer1 = pointer2;
         }
         pointer2 = pointer2.Next;
       }
-      
+
       return list;
+    }
+
+    public static int[] RangeCopy (this int[] arr, int start, int end) {
+      var x = new int[end - start + 1];
+      for (var i = start; i <= end; i++) {
+        x[i - start] = arr[i];
+      }
+      return x;
+    }
+
+    public static int[] Merge (int[] arr1, int[] arr2) {
+      var x = new int[arr1.Length + arr2.Length];
+      var i = 0;
+      var j = 0;
+      while (i < arr1.Length || j < arr2.Length) {
+        if (i == arr1.Length) {
+          x[i + j] = arr2[j];
+          j++;
+        } else if (j == arr2.Length) {
+          x[i + j] = arr1[i];
+          i++;
+        } else if (arr1[i] < arr2[j]) {
+          x[i + j] = arr1[i];
+          i++;
+        } else {
+          x[i + j] = arr2[j];
+          j++;
+        }
+      }
+      return x;
+    }
+
+    public static int[] MergeSort (this int[] arr, int left, int right) {
+      if (right <= left) return arr;
+      var middle = (left + right) / 2;
+      var sortedFirstHalf = MergeSort (arr.RangeCopy (0, middle - left), 0, middle - left);
+      var sortedSecondHalf = MergeSort (arr.RangeCopy (middle - left + 1, right - left), middle - left + 1, right - left);
+      return Merge (sortedFirstHalf, sortedSecondHalf);
     }
   }
 }
--- BasicDataStructure/test/UtilitiesTest.cs
+++ BasicDataStructure/test/UtilitiesTest.cs
@@ -7,43 +7,97 @@ namespace test {
     [Fact]
     public void RemoveDuplicates_GivenListWithoutDuplicates_ExpectSuccess () {
       //Given
-      var expected = new LinkedList();
-      expected.Head = new Node(0);
-      expected.Head.Next = new Node(1);
-      expected.Head.Next.Next = new Node(2);
-      expected.Head.Next.Next.Next = new Node(3);
+      var expected = new LinkedList ();
+      expected.Head = new Node (0);
+      expected.Head.Next = new Node (1);
+      expected.Head.Next.Next = new Node (2);
+      expected.Head.Next.Next.Next = new Node (3);
 
-      var list = new LinkedList();
-      list.Head = new Node(0);
-      list.Head.Next = new Node(1);
-      list.Head.Next.Next = new Node(2);
-      list.Head.Next.Next.Next = new Node(3);
+      var list = new LinkedList ();
+      list.Head = new Node (0);
+      list.Head.Next = new Node (1);
+      list.Head.Next.Next = new Node (2);
+      list.Head.Next.Next.Next = new Node (3);
 
       //When
-      var actual = Utilities.RemoveDuplicates(list);
+      var actual = Utilities.RemoveDuplicates (list);
 
       //Then
-      actual.Should().BeEquivalentTo(expected);
+      actual.Should ().BeEquivalentTo (expected);
     }
 
     [Fact]
     public void RemoveDuplicates_GivenListWithDuplicates_ExpectSuccess () {
       //Given
-      var expected = new LinkedList();
-      expected.Head = new Node(0);
-      expected.Head.Next = new Node(1);
-      expected.Head.Next.Next = new Node(2);
-      expected.Head.Next.Next.Next = new Node(3);
+      var expected = new LinkedList ();
+      expected.Head = new Node (0);
+      expected.Head.Next = new Node (1);
+      expected.Head.Next.Next = new Node (2);
+      expected.Head.Next.Next.Next = new Node (3);
 
-      var list = new LinkedList();
-      list.Head = new Node(0);
-      list.Head.Next = new Node(1);
-      list.Head.Next.Next = new Node(2);
-      list.Head.Next.Next.Next = new Node(2);
-      list.Head.Next.Next.Next.Next = new Node(3);
+      var list = new LinkedList ();
+      list.Head = new Node (0);
+      list.Head.Next = new Node (1);
+      list.Head.Next.Next = new Node (2);
+      list.Head.Next.Next.Next = new Node (2);
+      list.Head.Next.Next.Next.Next = new Node (3);
 
       //When
-      var actual = Utilities.RemoveDuplicates(list);
+      var actual = Utilities.RemoveDuplicates (list);
+
+      //Then
+      actual.Should ().BeEquivalentTo (expected);
+    }
+
+    [Fact]
+    public void Merge_GivenTwoArrays_ExpectBiggerArray () {
+      //Given
+      var arr1 = new [] { 1, 2, 3 };
+      var arr2 = new [] { 2, 4, 5 };
+      var expected = new [] { 1, 2, 2, 3, 4, 5 };
+
+      //When
+      var actual = Utilities.Merge (arr1, arr2);
+
+      //Then
+      actual.Should ().BeEquivalentTo (expected);
+    }
+
+    [Fact]
+    public void Merge_GivenTwoArrays_ExpectBiggerArray1 () {
+      //Given
+      var arr1 = new [] { 1, 3 };
+      var arr2 = new [] { 2, 4, 5 };
+      var expected = new [] { 1, 2, 3, 4, 5 };
+
+      //When
+      var actual = Utilities.Merge (arr1, arr2);
+
+      //Then
+      actual.Should ().BeEquivalentTo (expected);
+    }
+
+    [Fact]
+    public void RangeCopy_GivenValidRange_ExpectSubArray () {
+      //Given
+      var arr = new [] { 1, 2, 3, 4, 5 };
+      var expected = new [] { 2, 3, 4 };
+
+      //When
+      var actual = arr.RangeCopy (1, 3);
+
+      //Then
+      actual.Should ().BeEquivalentTo (expected);
+    }
+
+    [Fact]
+    public void MergeSort_GivenUnsortedArray_ExpectSortedArray () {
+      //Given
+      var arr = new [] { 30, 8, 0, 21, 5, 2, 11 };
+      var expected = new [] { 0, 2, 5, 8, 11, 21, 30 };
+
+      //When
+      var actual = arr.MergeSort(0, arr.Length - 1);
 
       //Then
       actual.Should().BeEquivalentTo(expected);
